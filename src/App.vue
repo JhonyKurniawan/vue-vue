3ca1222e61db8366450ffeb08dd2e9253d05ed3c
MayumiHikari778 <jhonykurniawan.blds@gmail.com>
2023-04-11 23:57:47 +0700

    a

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import Header from './components/Header.vue'
 
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#"><img src="logo-baju.png" style="width: 80px"></a>
+          <a class="navbar-brand" href="#"><img src="logo.png" style="width: 80px"></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
